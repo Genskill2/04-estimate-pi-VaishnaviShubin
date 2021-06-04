29622a1b2ffea4dba9f6466e1197d944a9cbd842
VaishnaviShubin <84711883+VaishnaviShubin@users.noreply.github.com>
2021-06-04 22:17:58 +0300

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -28,7 +28,7 @@ float wallis_pi(int n)
     float s=1.0;
     for (int i=1; i<=n; i++)
     {
-        s=s*(4*i*i)/((4*i*i)-1);
+        s=s*(4.0*i*i)/(float)((4.0*i*i)-1);
     }
-   return (2*s); 
+   return (2.0*s); 
  }
